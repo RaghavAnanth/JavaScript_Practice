66ea297cba07082ce072bdb1cfe5256843fcd569
Veera Ragavan A <raghavanand1028@gmail.com>
2024-05-09 15:42:28 +0530

    initial version

---
+++ JavaScript_Training/script.js
@@ -0,0 +1,11 @@
+const views = document.getElementsByClassName("view");
+console.log(views);
+
+
+view1.style.display="flex";
+const head = document.querySelector("nav");
+head.innerHTML='<h1> Hello World! </h1> <p>I\'m Ragavan</p>';
+console.log(head);
+
+view1.style.display="flex";
+view2.style.display="flex";
